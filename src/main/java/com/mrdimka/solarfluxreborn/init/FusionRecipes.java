392a1db128c28fe696a26a8c6784bd367a89ee93
Alexander Hess <github@xandr.co.uk>
2017-02-07 02:59:08 +0000

    Fix integer overflow
    The energyCost parameter of the SimpleFusionRecipe constructor is multiplied by the number of pedestals used. A cost of 1,000,000,000 will cause an integer overflow since the total cost is stored as signed 32bit integer. If the total crafting cost was supposed to be 1bn RF, 128,000,000 would be the correct amount in the function call. Alternatively change the amount to 256m to match the most expensive chaotic recipe in DE with 2bn RF.

--- src/main/java/com/mrdimka/solarfluxreborn/init/FusionRecipes.java
+++ src/main/java/com/mrdimka/solarfluxreborn/init/FusionRecipes.java
@@ -13,6 +13,6 @@ public class FusionRecipes
 	public static void register()
 	{
 		if(DraconicEvolutionConfigs.chaoticSolar && DraconicEvolutionConfigs.draconicSolar && DraconicEvolutionConfigs.useFusionForChaotic)
-		FusionRecipeAPI.addRecipe(new SimpleFusionRecipe(new ItemStack(ModBlocks.chaoticSolar, 3), new ItemStack(Item.REGISTRY.getObject(new ResourceLocation("draconicevolution", "chaos_shard"))), 1000000000, 3, ModBlocks.draconicSolar, Item.REGISTRY.getObject(new ResourceLocation("draconicevolution", "awakened_core")), ModBlocks.draconicSolar, Item.REGISTRY.getObject(new ResourceLocation("draconicevolution", "awakened_core")), ModBlocks.draconicSolar, Item.REGISTRY.getObject(new ResourceLocation("draconicevolution", "awakened_core")), ModBlocks.draconicSolar, Item.REGISTRY.getObject(new ResourceLocation("draconicevolution", "awakened_core"))));
+		FusionRecipeAPI.addRecipe(new SimpleFusionRecipe(new ItemStack(ModBlocks.chaoticSolar, 3), new ItemStack(Item.REGISTRY.getObject(new ResourceLocation("draconicevolution", "chaos_shard"))), 128000000, 3, ModBlocks.draconicSolar, Item.REGISTRY.getObject(new ResourceLocation("draconicevolution", "awakened_core")), ModBlocks.draconicSolar, Item.REGISTRY.getObject(new ResourceLocation("draconicevolution", "awakened_core")), ModBlocks.draconicSolar, Item.REGISTRY.getObject(new ResourceLocation("draconicevolution", "awakened_core")), ModBlocks.draconicSolar, Item.REGISTRY.getObject(new ResourceLocation("draconicevolution", "awakened_core"))));
 	}
 }
