3a63129e5a46a4e644185de7f240dbaf70be1f83
Zeith <me@zeith.org>
2022-09-04 18:35:56 +0300

    Updated to latest HammerLib.
    Signed-off-by: Zeith <me@zeith.org>

--- src/main/java/org/zeith/solarflux/recipe/RecipeClearSolarPanel.java
+++ src/main/java/org/zeith/solarflux/recipe/RecipeClearSolarPanel.java
@@ -3,8 +3,7 @@ package org.zeith.solarflux.recipe;
 import net.minecraft.core.NonNullList;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.crafting.Ingredient;
-import net.minecraft.world.item.crafting.ShapelessRecipe;
+import net.minecraft.world.item.crafting.*;
 import org.zeith.hammerlib.core.adapter.recipe.RecipeBuilder;
 import org.zeith.hammerlib.event.recipe.RegisterRecipesEvent;
 import org.zeith.solarflux.block.SolarPanelBlock;
@@ -16,41 +15,41 @@ public class RecipeClearSolarPanel
 	{
 		super(id, group, result, input);
 	}
-
+	
 	@Override
 	public boolean isSpecial()
 	{
 		return true;
 	}
-
+	
 	public static RecipeClearSolarPanelBuilder builder(RegisterRecipesEvent $)
 	{
 		return new RecipeClearSolarPanelBuilder($);
 	}
-
+	
 	public static class RecipeClearSolarPanelBuilder
-			extends RecipeBuilder<RecipeClearSolarPanelBuilder>
+			extends RecipeBuilder<RecipeClearSolarPanelBuilder, Recipe<?>>
 	{
 		private final NonNullList<Ingredient> ingredients = NonNullList.create();
-
+		
 		public RecipeClearSolarPanelBuilder(RegisterRecipesEvent event)
 		{
 			super(event);
 		}
-
+		
 		public RecipeClearSolarPanelBuilder set(SolarPanelBlock ingredient)
 		{
 			result(ingredient).ingredients.add(Ingredient.of(ingredient));
 			return this;
 		}
-
+		
 		@Override
 		public void register()
 		{
 			validate();
 			if(ingredients.isEmpty())
 				throw new IllegalStateException(getClass().getSimpleName() + " does not have any defined ingredients!");
-			event.add(new RecipeClearSolarPanel(getIdentifier(), group, result, ingredients));
+			event.register(getIdentifier(), new RecipeClearSolarPanel(getIdentifier(), group, result, ingredients));
 		}
 	}
 }
